d32e086b70def04bb2e57f0981923ffe9753837f
kawadaren <renka19975@gmail.com>
2019-08-27 16:24:56 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
