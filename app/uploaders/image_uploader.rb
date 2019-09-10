ae87dabea248ba077247266fba48fdca2ceb2810
kawadaren <renka19975@gmail.com>
2019-09-10 09:49:58 +0900

    secret-修正3-

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,9 @@
+# encoding: utf-8
+
 class ImageUploader < CarrierWave::Uploader::Base
+
   # Include RMagick or MiniMagick support:
-  # include ::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
