fc4c28e46a24a75c260c0fd9f5de1f5949ce5bce
kawadaren <renka19975@gmail.com>
2019-09-10 13:46:05 +0900

    secret-修正４-

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' 
   }
 
-  config.fog_directory  = 'chatspace2004'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace2004'
+  config.fog_directory  = 'chatspace-s'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-s'
 end
