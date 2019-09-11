c945eba8310c2f627480f5ae6a34373dacec5eb0
kawadaren <renka19975@gmail.com>
2019-09-12 00:05:08 +0900

    修正９

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -2,7 +2,7 @@
 $(function() {
   var search_result = $("#user-search-result");
   function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
